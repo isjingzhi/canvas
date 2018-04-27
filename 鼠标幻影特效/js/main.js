ebd691d3758704e2b97bf0d054893473673b059f
Jim <jinhengyu666@gmail.com>
2018-04-27 13:32:27 +0800

    参数锁定

--- 鼠标幻影特效/js/main.js
+++ 鼠标幻影特效/js/main.js
@@ -1,94 +1,68 @@
-    let c = document.getElementById("c");
-    let ctx = c.getContext("2d");
-    let w, h, cx, cy, l;
-    let y = [];
-    let b = {
-        n: 100,
-        c: false,    //  颜色  如果是false 则是随机渐变颜色
-        bc: '#000',   //  背景颜色
-        r: 0.9,     //radius
-        o: 0.05,    //opacity
-        a: 1,
-        s: 20,
-    };
-    let bx = 0, by = 0, vx = 0, vy = 0;
-    let td = 0;
-    let p = 0;
-    let hs = 0;
-    resize();
-    let color, color2;
-    if (b.c) {
-        color2 = b.c;
-    } else {
-        color = Math.random() * 360;
+let c = document.getElementById("c");
+let ctx = c.getContext("2d");
+let w, h, cx, cy, l;
+let particles = [];     //存放所有粒子
+let b = {
+    // n: 100,
+    // a: 1,
+    // s: 20,
+};
+let bgColor = '#000';  //背景颜色
+let radiusDelta = .9;     //radius增量
+let opacityDelta = -.03;   //opacity增量
+// let bx = 0, by = 0;
+// vx = 0, vy = 0;
+// let td = 0;
+// let p = 0;
+// let hs = 0;
+let color;
+let hue = Math.random() * 360;
+let hueDelta = .2;    //hue增量
+
+window.onresize = resize;
+resize();
+
+function begin() {
+    hue += hueDelta;
+    color = `hsl(${hue},100%,80%)`;
+    ctx.globalAlpha = 1;
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, w, h);
+    for (let p of particles) {
+        ctx.globalAlpha = p.o;
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+        p.r += radiusDelta;
+        p.o += opacityDelta;
     }
+    //过滤掉opacity<=0的
+    particles = particles.filter((p) => p.o > 0);   //map和filter并不改变原数组
+    window.requestAnimationFrame(begin);
+}
 
-    window.onresize = resize;
+function resize() {
+    w = window.innerWidth;
+    h = window.innerHeight;
+    c.width = w;
+    c.height = h;
+    cx = w / 2;
+    cy = h / 2;
+}
 
-    function begin() {
+c.onmousemove = function (e) {
+    cx = e.clientX - c.offsetLeft;
+    cy = e.clientY - c.offsetTop;
 
-        if (!b.c) {
-            color += .1;
-            color2 = 'hsl(' + color + ',100%,80%)';
-        }
-        ctx.globalAlpha = 1;
-        ctx.fillStyle = b.bc;
-        ctx.fillRect(0, 0, w, h);
-        for (let i = 0; i < y.length; i++) {
-            ctx.globalAlpha = y[i].o;
-            ctx.fillStyle = color2;
-            ctx.beginPath();
-            ctx.arc(y[i].x, y[i].y, y[i].r, 0, Math.PI * 2);
-            ctx.closePath();
-            ctx.fill();
-            y[i].r += b.r;
-            y[i].o -= b.o;
-            if (y[i].o <= 0) {
-                y.splice(i, 1);
-                i--;
-            }
-        }
-        window.requestAnimationFrame(begin);
-    }
+    particles.push({
+        x: cx,
+        y: cy,
+        r: .1,
+        o: 1,
+        // v: 0
+    });
+};
 
-    function resize() {
-        w = window.innerWidth;
-        h = window.innerHeight;
-        c.width = w;
-        c.height = h;
-        cx = w / 2;
-        cy = h / 2;
-    }
-
-    c.onmousemove = function (e) {
-        cx = e.clientX - c.offsetLeft;
-        cy = e.clientY - c.offsetTop;
-
-        y.push({
-            x: cx,
-            y: cy,
-            r: b.r,
-            o: 1,
-            v: 0
-        });
-
-    };
-    /*c.mousedown(function(){
-     c.on('mousemove',function(e){
-     cx = e.pageX-c.offset().left;
-     cy = e.pageY-c.offset().top;
-     y.push({x:cx,y:cy,r:b.r,o:1});
-     });
-     c.on('mouseup',function(){
-     c.off('mouseup');
-     c.off('mousemove');
-     c.off('moseleave');
-     });
-     c.on('mouseleave',function(){
-     c.off('mouseup');
-     c.off('mousemove');
-     c.off('moseleave');
-     });
-     });*/
-
-    begin();
+begin();
