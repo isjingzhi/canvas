b68880523b10b2b5c171793289dc95429254711d
Jim <jinhengyu666@gmail.com>
2018-04-28 20:46:26 +0800

    新项目!!

--- 各色图章幻影特效/js/main.js
+++ 各色图章幻影特效/js/main.js
@@ -5,18 +5,13 @@ let particles = [];     //存放所有粒子
 let bgColor = '#000';  //背景颜色
 let radiusDelta = .9;     //radius增量
 let opacityDelta = -.03;   //opacity增量
+let hueDelta = [-.2, .2][~~(Math.random() * 2)];    //hue增量
 let color;      //瞬时颜色
 let hue = Math.random() * 360;
-let hueDelta = [-.2, .2][~~(Math.random() * 2)];    //hue增量
 let shape = 'square';   //图案的形状
-let isHollow = true;
+let isHollow = true;    //是否空心
 let rt2 = Math.sqrt(2);
 let d45 = Math.PI / 4;
-
-(window.onresize = () => {
-    c.width = w = window.innerWidth;
-    c.height = h = window.innerHeight;
-})();
 
 class Particle {
     constructor(xx, yy) {
@@ -34,8 +29,9 @@ c.onmousemove = (e) => {
 let draw = ({x, y, r, o}) => {   //参数解构
     switch (shape) {
         case 'square': {
-            if (isHollow) ctx.strokeRect(x - r, y - r, r * 2, r * 2);
-            else ctx.fillRect(x - r, y - r, r * 2, r * 2);
+            let R = r * 2;
+            if (isHollow) ctx.strokeRect(x - r, y - r, R, R);
+            else ctx.fillRect(x - r, y - r, R, R);
             break;
         }
         case 'circle': {
@@ -47,23 +43,23 @@ let draw = ({x, y, r, o}) => {   //参数解构
             break;
         }
         case 'cross': {
-            r *= 1.2;
+            let R = r * 1.2;
             if (isHollow) {
-                ctx.strokeRect(x - r, y - r / 6, r * 2, r / 3);
-                ctx.strokeRect(x - r / 6, y - r, r / 3, r * 2);
+                ctx.strokeRect(x - R, y - R / 6, R * 2, R / 3);     //JS中:5/2===2.5 ....
+                ctx.strokeRect(x - R / 6, y - R, R / 3, R * 2);
             } else {
-                ctx.fillRect(x - r, y - r / 6, r * 2, r / 3);
-                ctx.fillRect(x - r / 6, y - r, r / 3, r * 2);
+                ctx.fillRect(x - R, y - R / 6, R * 2, R / 3);
+                ctx.fillRect(x - R / 6, y - R, R / 3, R * 2);
             }
             break;
         }
         case 'concave': {
-            let R = r * rt2, r2 = r * 2;
+            let R = r * rt2, RR = r * 2;
             ctx.beginPath();
-            ctx.arc(x - r2, y, R, -d45, d45);
-            ctx.arc(x, y + r2, R, -3 * d45, -d45);
-            ctx.arc(x + r2, y, R, 3 * d45, 5 * d45);
-            ctx.arc(x, y - r2, R, d45, 3 * d45);
+            ctx.arc(x - RR, y, R, -d45, d45);
+            ctx.arc(x, y + RR, R, -3 * d45, -d45);
+            ctx.arc(x + RR, y, R, 3 * d45, 5 * d45);
+            ctx.arc(x, y - RR, R, d45, 3 * d45);
             ctx.closePath();
             if (isHollow) ctx.stroke();
             else ctx.fill();
@@ -102,6 +98,25 @@ let draw = ({x, y, r, o}) => {   //参数解构
     }
 };
 
+document.querySelectorAll('#top-left button').forEach((item) => {
+    item.addEventListener("click", () => {
+        shape = item.value;   //利用闭包
+        document.querySelector('#top-left .active').classList.remove('active');
+        item.classList.add('active');
+    })
+});
+let toggleHollow = () => {
+    if (!event.target.classList.contains('active')) {   //event可以在onclick方法作用域中使用
+        document.querySelector('#down-left .active').classList.remove('active');
+        event.target.classList.add('active');
+        isHollow = !isHollow;
+    }
+};
+
+(window.onresize = () => {
+    c.width = w = window.innerWidth;
+    c.height = h = window.innerHeight;
+})();
 
 (function begin() {
     hue += hueDelta;
@@ -122,18 +137,3 @@ let draw = ({x, y, r, o}) => {   //参数解构
     particles = particles.filter((p) => p.o > 0);   //map和filter并不改变原数组
     window.requestAnimationFrame(begin);
 })();
-
-document.querySelectorAll('#top-left button').forEach((item) => {
-    item.addEventListener("click", () => {
-        shape = item.value;   //利用闭包
-        document.querySelector('#top-left .active').classList.remove('active');
-        item.classList.add('active');
-    })
-});
-let toggleHollow = () => {
-    if (!event.target.classList.contains('active')) {   //event可以在onclick方法作用域中使用
-        document.querySelector('#down-left .active').classList.remove('active');
-        event.target.classList.add('active');
-        isHollow = !isHollow;
-    }
-};
---
+++ 艺术字符涂鸦/main.js
@@ -0,0 +1,91 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let counter = 0;
+const minFontSize = 3;
+const mouse = {x: 0, y: 0, down: false};
+const position = {x: 0, y: window.innerHeight / 2};     //应该是上一次鼠标的位置
+const letters = "坚持一件你认为有价值的事情!";
+const angleDistortion = 0; //角变形
+
+
+function mouseMove(event) {
+    mouse.x = event.pageX; //鼠标指针的位置
+    mouse.y = event.pageY;
+    draw();
+}
+
+function mouseUp(event) {
+    mouse.down = false;
+}
+
+function draw() {
+    if (mouse.down) {
+        const letter = letters[counter];
+        const d = distance(position, mouse);
+        const fontSize = minFontSize + d / 2;
+        const stepSize = textWidth(letter, fontSize);
+
+        if (d > stepSize) {
+            //返回从X轴正向逆时针到（x,y）点的角度
+            const angle = Math.atan2(mouse.y - position.y, mouse.x - position.x);
+            ctx.font = `${fontSize}px Georgia`; //定义字号，字体
+            ctx.fillStyle = "magenta";
+            ctx.save(); //保存当前的环境状态
+            ctx.translate(position.x, position.y); //将新的画布（0,0）改为（x,y）
+            ctx.rotate(angle); //
+            ctx.fillText(letter, 0, 0); //开始绘制文本时，相对于文本的位置
+            ctx.restore(); //返回之前保存过的路径状态和属性。
+
+            counter++;
+            if (counter > letters.length - 1) {
+                counter = 0;
+            }
+            position.x = position.x + Math.cos(angle) * stepSize;
+            position.y = position.y + Math.sin(angle) * stepSize;
+        }
+    }
+}
+
+function mouseDown(event) {
+    mouse.down = true;
+    position.x = event.pageX;
+    position.y = event.pageY;
+    // document.getElementById("info")
+}
+
+//决定绘制文本字体大小
+//当鼠标按下时，获得鼠标移动前的x,y坐标，传入pt中
+//当鼠标移动时，获得鼠标移动后的x,y坐标，传入pt2中
+//通过获取这两个状态的坐标，来设置文字的大小，
+//与函数textWidth()函数共同，表现出一种，速度越快，文字越大的效果
+function distance(pt, pt2) {
+    let xs;
+    let ys;
+    xs = pt2.x - pt.x;
+    xs = xs * xs;
+    ys = pt2.y - pt.y;
+    ys = ys * ys;
+    return Math.sqrt(xs + ys);
+}
+
+function textWidth(string, size) {
+    ctx.font = size + "px Georgia"; //定义字号，字体
+    if (ctx.fillText) {
+        return ctx.measureText(string).width; //该函数以像素指定文字的宽度。
+    } else if (ctx.mozDrawText) {
+        return ctx.mozMeasureText(string);
+    }
+}
+
+(window.onresize = function (event) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})();
+
+canvas.addEventListener('mousemove', mouseMove, false);
+canvas.addEventListener('mousedown', mouseDown, false);
+canvas.addEventListener('mouseup', mouseUp, false);
+canvas.addEventListener('mouseout', mouseUp, false);
+
+(function init() {
+})();
