2640b804b7c722703c8e99a9f0ca385212bd2665
Jim <jinhengyu666@gmail.com>
2018-04-30 10:00:47 +0800

    更新抖动间隔时间

--- 彩虹波澜心电图/js/index.js
+++ 彩虹波澜心电图/js/index.js
@@ -31,6 +31,8 @@ let initCanvas = function () {
     h = c.height = trimUnit(s.height, 'px');
 };
 
+let positiveCosine = (x) => cos(x) > 0 ? cos(x) : 0;
+
 let wave = function () {
     x0 = -1;
     y0 = h / 2;
@@ -44,7 +46,7 @@ let wave = function () {
         y = 9 * sqrt(x) * sin(x / 23 / PI + t / 3 + sin(x / 29 + t)) +
             32 * sin(t) * cos(x / 19 + t / 7) +
             //     抖动周期                    振幅大小
-            16 * cos(t / period) * sin(sqrt(x) + rand(3, 2) * tmp) +      //这一项决定了抖动偏移值
+            16 * positiveCosine(t / period) * sin(sqrt(x) + rand(3, 2) * tmp) +      //这一项决定了抖动偏移值
             h / 2;
 
         ctx.beginPath();
@@ -74,10 +76,10 @@ setTimeout(function () {
 
 addEventListener('keydown', () => {
     t_step = 1 / 10;
-    period=4;
+    period = 4;
 });
 
 addEventListener('keyup', () => {
     t_step = 1 / 20;
-    period=2;
+    period = 2;
 });
