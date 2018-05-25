57a54498e5e7cd1995765b71ce355645145c9f73
Jim <jinhengyu666@gmail.com>
2018-05-25 13:56:19 +0800

    我好害怕...
    加入了很多fancy的语法

--- 3d-fear表情包/main.js
+++ 3d-fear表情包/main.js
@@ -1,70 +1,53 @@
-let timer = null;
-
 const cub = document.getElementById('scene');
-const f = document.getElementById('front');
-const a = document.getElementById('after');
-const l = document.getElementById('left');
-const r = document.getElementById('right');
-const t = document.getElementById('top');
-const b = document.getElementById('bottom');
-
+//高效
+const [f, a, l, r, t, b] = document.querySelectorAll(`.shape .face .photon-shader`);
 
 window.onload = function () {
-    timer = setTimeout(function () {
-        reader();
-        setTimeout(arguments.callee, 16.7);
-    }, 16.7);
+    window.reader();
+    // arguemts.callee指代的是所在函数体对应的函数
+    // 队规调用+立即执行不再需要函数名了...
+    // 所以这里不能用箭头函数..
+    window['timer'] = requestAnimationFrame(arguments.callee);  // 很fancy的写法
 };
 
+// 模块化编程
 (function () {
-    let scale = 0, rotateX = 0, speed = 2, n = 0, m = 0;
-    let flag = true;
+    let scale = 0,
+        rotateX = 0,
+        speed = 2,
+        n = 0,
+        m = 0,
+        flag = true;
 
     function reader() {
         const patten = /-?\d+\.?\d*/g;
         const rotate = cub.style.transform.match(patten);
-        // 形态1
-        if (flag) {
-            if (+rotate[4] > 0.6) {
+        if (flag) { // 形态1
+            if (+rotate[4] > 0.6)
                 scale = (+rotate[4] - 0.002)
-            }
-            else {
-                flag = false;
-            }
-            if (+rotate[0] > -15) {
+            else flag = false;
+            if (+rotate[0] > -15)
                 rotateX = +rotate[0] - 0.05;
-            }
-            if (speed > 0) {
-                speed = speed - 0.0008;
-            }
-        }
-        //切换形态2
-        else {
-            if (+rotate[4] < 5) {
+            if (speed > 0) speed = speed - 0.0008;
+        } else { //切换形态2
+            if (+rotate[4] < 5)
                 scale = (+rotate[4] + 0.008)
-            }
-            if (+rotate[0] > -15) {
+            if (+rotate[0] > -15)
                 rotateX = +rotate[0] - 0.05;
-            }
-            if (speed > 0) {
-                speed = speed - 0.0085;
-            }
-            if (n < 120) {
-                n++;
-            }
-            if (m < 90) {
-                m = m + 0.5;
-            }
+            if (speed > 0) speed = speed - 0.0085;
+            if (n < 120) n++;
+            if (m < 90) m = m + 0.5;
             else {
                 speed = 0;
-                clearTimeout(timer);
+                cancelAnimationFrame(window.timer);
             }
-            t.style.transform = 'rotateY(' + n + 'deg)';
-            a.style.transform = 'rotateX(' + -m + 'deg)';
-            l.style.transform = 'rotateX(' + -m + 'deg)';
+            t.style.transform = `rotateY(${n}deg)`;
+            a.style.transform = `rotateX(${-m}deg)`;
+            l.style.transform = `rotateX(${-m}deg)`;
         }
-        cub.style.transform = "rotateX(" + (rotateX) + "deg) rotateY(" + (+rotate[1] - speed) + "deg) scale3d(" + scale + "," + scale + "," + scale + ")";
+        cub.style.transform = `rotateX(${rotateX}deg) rotateY(${+rotate[1] - speed}deg) scale3d(${scale}, ${scale}, ${scale})`;
     }
 
+    //利用window共享全局对象
     return window['reader'] = reader;
 })();
